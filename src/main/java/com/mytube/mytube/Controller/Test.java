31a5238e9a3701dd579f264d674072b045eb5cb2
leeyeonji28 <yeon102800@gmail.com>
2022-09-11 23:27:37 +0900

    back setting

---
+++ src/main/java/com/mytube/mytube/Controller/Test.java
@@ -0,0 +1,14 @@
+package com.mytube.mytube.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Test {
+    @RequestMapping("/")
+    @ResponseBody
+    public String index(){
+        return "index";
+    }
+}
---
+++ src/test/java/com/mytube/mytube/MyTubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytube.mytube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
